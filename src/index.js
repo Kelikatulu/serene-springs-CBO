fcb642c202ee97da89f80d8596454d814c0298c4
Kelikatulu <hellkelly136@gmail.com>
2025-12-01 16:57:28 +0300

    index.js
    Updated import statements to use double quotes and changed the CSS import path.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/style.css"; // optional custom styles
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
